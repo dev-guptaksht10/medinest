82502908318d709c66a1a16525bc87139cbac3f2
Krishan-Kant123 <aalokyadav222@gmail.com>
2025-04-03 19:00:44 +0530

    r5

--- client/src/utils/axiosInstance.ts
+++
@@ -1,8 +0,0 @@
-import axios from "axios";
-
-const axiosInstance = axios.create({
-    baseURL: "http://localhost:4444/api/users", 
-    withCredentials: true, 
-});
-
-export default axiosInstance;
